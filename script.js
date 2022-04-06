2023ff365a8900b21c0630780a345d2537aeaa84
Dmitriy <dima.korn1996@gmail.com>
2022-04-06 16:30:26 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// "use strict";
+
+// let number = 4;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = "Alex";
+
+// const bool = false;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "Alex",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.png0', {}, []];
+// console.log(arr[1]);
---
+++ ss.js
@@ -0,0 +1,45 @@
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 2);
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваше фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// console.log(answers);
+
+
+// const category = "toys";
+// console.log(`https://someurl.com/${category}/5`);
+
+const user = "Alex";
+
+alert(`Привет, ${user}`);
+
+console.log("arr" + " - object");
+
+let incr =  10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+
+const x = true,
+    y= true;
+
+console.log(x && !y);
+
+const age = 20;
+
+console.log(`age ${"лет"}`);
